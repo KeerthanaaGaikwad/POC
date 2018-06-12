89ba2718af0b4062ac44f2440d21c96c15703175
k.sainathshobha <k.sainathshobha@cgi.com>
2018-06-12 11:00:41 -0400

    changing content type

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.controller('MainController',['$scope','$http', function($scope, $http) {
 			url: 'https://upload.box.com/api/2.0/files/content',
 			withCredentials : true,
 			headers:{ 
-				'Authorization':  'Bearer ' + "OHcXAfDRyy0JNuhQ9WxFqoyc2tMfYFEJ",
+				'Authorization':  'Bearer ' + "xYhKekAk2WLXV6KnSlGcyEV7KkeWK36Y",
 				'Access-Control-Allow-Origin': '*',
 				'Content-Type': 'multipart/form-data'
 			},
