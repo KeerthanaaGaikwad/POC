ea243f9e66089c929ccb139697f418c93c1120c8
k.sainathshobha <k.sainathshobha@cgi.com>
2018-06-12 12:23:05 -0400

    changing content type

--- app.js
+++ app.js
@@ -80,9 +80,11 @@ app.controller('MainController',['$scope','$http', function($scope, $http) {
 				'Access-Control-Allow-Origin': '*',
 				'Content-Type': 'multipart/form-data'
 			},
-			name : files[0].name,
-			parent : {
-				id : 0
+			data : {
+				name : files[0].name,
+				parent : {
+					id : 0
+				}
 			}
 		}).then(function(){
 			console.log("success");
